06610b612ad0ac3fbcae70b7d94bc91f525c406c
shevsan <sasha41411@gmail.com>
2022-06-01 21:13:30 +0300

    test bnb

--- app/src/main/java/com/example/mazano/MainActivity.kt
+++ app/src/main/java/com/example/mazano/MainActivity.kt
@@ -1,45 +1,30 @@
 package com.example.mazano
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.mazano.databinding.ActivityMainBinding
-import com.example.mazano.ui.books.BooksFragment
-import com.example.mazano.ui.games.GamesFragment
-import com.example.mazano.ui.movies.MoviesFragment
-import com.ismaeldivita.chipnavigation.ChipNavigationBar
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var menu:ChipNavigationBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,GamesFragment()).commit()
-        menu = findViewById(R.id.menu)
-        menu.setItemSelected(R.id.games, true)
         initBottomNavBar()
 
     }
 
     private fun initBottomNavBar() {
-        menu.setOnItemSelectedListener {
-            var fragment: Fragment? = null
-            when(it){
-                R.id.games -> fragment = GamesFragment()
-                R.id.movies -> fragment = MoviesFragment()
-                R.id.books -> fragment = BooksFragment()
-            }
-            supportFragmentManager.beginTransaction().replace(R.id.fragmentContainerView,fragment!!).commit()
-
-        }
-
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+        val fragmentContainerView = findViewById<BottomNavigationView>(R.id.bottomNavBar)
+        fragmentContainerView.setupWithNavController(navController)
     }
 
 
