9ce092817c8496f886632f0f70f1b7aa757f2eb4
BogdanStarunskiy <bogdan.starunskiy@gmail.com>
2022-06-01 13:36:18 +0300

    dependency injection

--- app/src/main/java/com/example/mazano/MainActivity.kt
+++ app/src/main/java/com/example/mazano/MainActivity.kt
@@ -1,11 +1,8 @@
 package com.example.mazano
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-
 import com.example.mazano.databinding.ActivityMainBinding
 import com.example.mazano.ui.books.BooksFragment
 import com.example.mazano.ui.games.GamesFragment
@@ -15,7 +12,6 @@ import com.ismaeldivita.chipnavigation.ChipNavigationBar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var navController: NavController
     private lateinit var menu:ChipNavigationBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
